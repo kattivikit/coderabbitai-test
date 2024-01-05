18cd7729d2665d5834341e8f75f116169bec3f60
kattivikit <mascialessandro89@gmail.com>
2024-01-05 16:35:53 +0100

    add book controller

---
+++ src/main/java/it/kattivikit/coderabbitaitest/rest/BookController.java
@@ -0,0 +1,25 @@
+package it.kattivikit.coderabbitaitest.rest;
+
+import it.kattivikit.coderabbitaitest.models.Book;
+import it.kattivikit.coderabbitaitest.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping(value="/books", produces = { MediaType.APPLICATION_JSON_VALUE })
+    public Mono<ResponseEntity<List<Book>>> getAllBooks() {
+        return bookService.getAllBooks()
+                .map(list -> new ResponseEntity<List<Book>>(list, HttpStatus.OK));
+    }
+}
+
