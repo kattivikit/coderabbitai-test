b598ac202a6b8cd19fd5a84bfcf8e2cb6d6a8c8a
kattivikit <mascialessandro89@gmail.com>
2024-01-12 10:24:29 +0100

    add book controller empty test

---
+++ src/test/java/it/kattivikit/coderabbitaitest/rest/BookControllerTest.java
@@ -0,0 +1,12 @@
+package it.kattivikit.coderabbitaitest.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookControllerTest {
+
+    @Test
+    void getAllBooks() {
+    }
+}
